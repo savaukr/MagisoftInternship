ef927a35593a2f4ecc1de7c48209fcc97303c394
Slava <sava_ukr@ukr.net>
2020-02-13 14:27:05 +0200

    webpack-setup add

---
+++ Webpack-Setup/src/app/index.js
@@ -0,0 +1,12 @@
+import '../style/app.scss';
+class TestClass {
+    constructor() {
+        let msg = "Using ES2015+ syntax!";
+        alert(msg);
+        console.log(msg);
+    }
+}
+let test = new TestClass();
+console.log('API Key from Define Plugin:', API_KEY);
+alert('API Key from Define Plugin:'+API_KEY);
+
