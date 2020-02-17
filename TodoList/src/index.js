1b7942553ac7af46c7d6eb78b9dc7974fc02ece6
Slava <sava_ukr@ukr.net>
2020-02-17 18:11:04 +0200

    to do list

---
+++ TodoList/src/index.js
@@ -0,0 +1,8 @@
+
+//import Parser from './scripts/parser';
+//import { add } from './scripts/core';
+
+import './styles/style.scss';
+
+
+console.log('todo list);
---
+++ practicingJSskill/sortObject/sortObject.js
@@ -0,0 +1,9 @@
+function sortObject(obj, key) {
+
+}
+let myArray = [ 
+	{ name: "Joe Blow", date: "Mon Oct 31 2016 00:00:00 GMT-0700 (PDT)" },
+	{ name: "Sam Snead", date: "Sun Oct 30 2016 00:00:00 GMT-0700 (PDT)" },
+	{ name: "John Smith", date: "Sat Oct 29 2016 00:00:00 GMT-0700 (PDT)"}
+];
+alert(new Date(myArray[0]).valueOff());
