f20c049c596548ccbcdadd6f593d74d021135a27
Slava <sava_ukr@ukr.net>
2020-05-05 18:13:47 +0300

    router

--- coursehunter/src/App.js
+++ coursehunter/src/App.js
@@ -4,10 +4,13 @@ import Header from './components/Header/Header'
 import RandomPlanet from './components/Randomplanet/Randomplanet';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.js'
 import {PeoplePage, PlanetsPage, StarshipsPage} from './components/Pages/'
+import {StarshipDetails} from './components/swComponents/'
 import SwapiService from './services/swapiService'
 import DummySwapiService from './services/swapiService'
 
 import { SwapiServiceProvider } from './components/swapiServiceContext/'
+
+import {BrowserRouter as Router, Route} from 'react-router-dom' 
 
 export default class App extends Component {
   
@@ -38,13 +41,21 @@ export default class App extends Component {
     return (
       <ErrorBoundary>
         <SwapiServiceProvider value={this.state.swapiService}>
-          <div className="App stardb-app">
-            <Header onServiceChange={this.onServiceChange}/>
-            {/*<RandomPlanet updateInterval={12000}/>*/}
-            <PeoplePage />
-            <PlanetsPage />
-            <StarshipsPage />
-          </div>
+          <Router>
+            <div className="App stardb-app">
+              <Header onServiceChange={this.onServiceChange}/ >
+              <RandomPlanet updateInterval={120000} />
+              <Route path="/" exact render={ () => <h2>Welcome to StarDB</h2> }  />
+              <Route path="/people"  component={PeoplePage} />
+              <Route path="/planets" component={PlanetsPage} />
+              <Route path="/starships" exact component={StarshipsPage} />
+              <Route path="/starships/:id"
+                     render= { ( {match} ) => {
+                       const { id } = match.params;
+                       return <StarshipDetails itemId={id} />
+                     }}/>
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundary>
     )
