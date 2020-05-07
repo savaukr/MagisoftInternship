601c9fd625269b6fa816c898b1cf7eaea0286e93
Slava <sava_ukr@ukr.net>
2020-05-07 12:37:16 +0300

    switch and redirect

--- coursehunter/src/App.js
+++ coursehunter/src/App.js
@@ -3,22 +3,27 @@ import './App.css';
 import Header from './components/Header/Header'
 import RandomPlanet from './components/Randomplanet/Randomplanet';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.js'
-import {PeoplePage, PlanetsPage, StarshipsPage} from './components/Pages/'
+import {PeoplePage, PlanetsPage, StarshipsPage, SecretPage, LoginPage} from './components/Pages/'
 import {StarshipDetails} from './components/swComponents/'
 import SwapiService from './services/swapiService'
 import DummySwapiService from './services/swapiService'
 
 import { SwapiServiceProvider } from './components/swapiServiceContext/'
 
-import {BrowserRouter as Router, Route} from 'react-router-dom' 
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom' 
 
 export default class App extends Component {
   
   state = {
     selectedPerson:2,
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
+    isLoggedIn: false
   }
-  
+  onLogin = () => {
+    this.setState({
+      isLoggedIn: true
+    })
+  }
   componentDidCatch() {
     this.setState({hasError: true});
   }
@@ -38,6 +43,7 @@ export default class App extends Component {
   }
 
   render() {   
+    const {isLoggedIn} = this.state
     return (
       <ErrorBoundary>
         <SwapiServiceProvider value={this.state.swapiService}>
@@ -45,15 +51,31 @@ export default class App extends Component {
             <div className="App stardb-app">
               <Header onServiceChange={this.onServiceChange}/ >
               <RandomPlanet updateInterval={120000} />
-              <Route path="/" exact render={ () => <h2>Welcome to StarDB</h2> }  />
-              <Route path="/people/:id?"  component={PeoplePage} />
-              <Route path="/planets" component={PlanetsPage} />
-              <Route path="/starships" exact component={StarshipsPage} />
-              <Route path="/starships/:id"
-                     render= { ( {match} ) => {
-                       const { id } = match.params;
-                       return <StarshipDetails itemId={ id }/>
-                     }} />
+              <Switch>
+                <Route path="/" exact render={ () => <h2>Welcome to StarDB</h2> }  />
+                <Route path="/people/:id?"  component={PeoplePage} />
+                <Route path="/planets" component={PlanetsPage} />
+                <Route path="/starships" exact component={StarshipsPage} />
+                <Route path="/starships/:id"
+                       render= { ( {match} ) => {
+                         const { id } = match.params;
+                         return <StarshipDetails itemId={ id }/>
+                       }}
+                />
+                 <Route path="/login" render= {() => (
+                                       <LoginPage 
+                                         isLoggedIn={ isLoggedIn }
+                                         onLogin={ this.onLogin }
+                                       />
+                                      ) } />
+                  <Route path="/secret" render={() => (
+                                          <SecretPage 
+                                            isLoggedIn={isLoggedIn}
+                                          />
+                                        ) } />
+                  {/*<Redirect to=""/>*/}
+                  <Route render={ () => <h2>Page not found</h2> } />
+              </Switch>
             </div>
           </Router>
         </SwapiServiceProvider>
--- coursehunter/src/components/Pages/index.js
+++ coursehunter/src/components/Pages/index.js
@@ -1,9 +1,13 @@
 import PeoplePage from './PeoplePage.js'
 import PlanetsPage from './PlanetsPage.js'
 import StarshipsPage from './StarshipsPage.js'
+import SecretPage from './SecretPage.js'
+import LoginPage from './LoginPage.js'
 
 export {
 	PeoplePage,
 	PlanetsPage,
-	StarshipsPage
+	StarshipsPage,
+	SecretPage,
+	LoginPage
 }
